15e3b1bb3f38a4f2b8e6a5def880d3bb221a3b83
yanzyuyu <stockrii800@gmail.com>
2026-09-12 19:36:23 +0700

    init package skeleton and config

---
+++ config/filament-wilayah.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'tables' => [
+        'provinces' => 'wilayah_provinces',
+        'cities' => 'wilayah_cities',
+        'districts' => 'wilayah_districts',
+        'villages' => 'wilayah_villages',
+    ],
+
+    'cache' => [
+        'enabled' => true,
+        'ttl_seconds' => 86400,
+    ],
+];
